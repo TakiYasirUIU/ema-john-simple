abbc58aa3fe6b6dbfdf3d35c7d41b968304aa4c4
TakiYasirUIU <yasirtaki1995@gmail.com>
2022-08-21 00:45:30 +0600

    Handle queantity and explanation

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -17,18 +17,34 @@ const Shop = () => {
 
     },[]);
 
-    useEffect(()=>{
+    // useEffect(()=>{
+    //     const storedCart = getStoredCart();
+    //     const savedCart = [];
+    //     for(const id in storedCart){
+    //         const addedProduct = products.find(product=>product.id === id);
+    //         if(addedProduct){
+    //             const quantity = storedCart[id];
+    //             addedProduct.quantity = quantity;
+    //             savedCart.push(addedProduct)
+    //         }
+    //     }
+    //      setCart(savedCart)
+    // },[products])
+
+    useEffect (()=>{
         const storedCart = getStoredCart();
         const savedCart = [];
         for(const id in storedCart){
-            const addedProduct = products.find(product=>product.id === id);
+            const addedProduct = products.find(product => product.id === id);
             if(addedProduct){
                 const quantity = storedCart[id];
                 addedProduct.quantity = quantity;
-                savedCart.push(addedProduct)
+                savedCart.push(addedProduct);
+
             }
         }
-         setCart(savedCart)
+        setCart(savedCart)
+
     },[products])
 
     const handleAddToCart = (product) =>{
